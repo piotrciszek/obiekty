7491f355f1a15098e8eaf5204b79a13c306cbda4
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-07 19:55:45 +0100

    Zadanie "data"

---
+++ src/main/java/zadaniaObiekty/data/Main.java
@@ -0,0 +1,13 @@
+package zadaniaObiekty.data;
+
+public class Main {
+    public static void main(String[] args) {
+        MojaData mojaData = new MojaData(1,11,2011);
+
+        System.out.println(mojaData.wyswietlDateA());
+        System.out.println(mojaData.wyswietlDateB());
+        System.out.println(mojaData.wyswietlDateC());
+
+    }
+
+}
