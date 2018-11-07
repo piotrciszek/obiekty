53f7d9bebabcdda7f7a1ee0654f56112cc53edf1
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-07 19:55:25 +0100

    Zadanie "samochod"

--- src/main/java/zadaniaObiekty/samochod/Main.java
+++ src/main/java/zadaniaObiekty/samochod/Main.java
@@ -3,14 +3,14 @@ package zadaniaObiekty.samochod;
 public class Main {
     public static void main(String[] args) {
 
-        Samochod opel = new Samochod("Czerwony",2010,"Opel");
-        SuperSamochod ope1l = new SuperSamochod("Czerwony",2010,"Opel");
-        SuperSamochod nieJakisTamOpel = new SuperSamochod("Zielony",2015,"Fiat 126p");
+        Samochod opel = new Samochod("Czerwony", 2010, "Opel");
+        SuperSamochod ope1l = new SuperSamochod("Czerwony", 2010, "Opel");
+        SuperSamochod nieJakisTamOpel = new SuperSamochod("Zielony", 2015, "Fiat 126p");
 
 
         System.out.println(ope1l.equals(opel));
 
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             nieJakisTamOpel.przyspiesz(200);
         }
 
